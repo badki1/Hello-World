67daf5094e939a9e27d5df74cf57fe6bdf0ce6ae
JAIN Ankush <ankush.jain2@soprasteria.com>
2019-12-20 16:41:06 +0530

    updated

--- app.py
+++ app.py
@@ -1,11 +1,1 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def index():
-    return "HELLO_WORLD"
-
-if __name__ == "__main__":
-    app.run(port=9000, use_reloader=True, debug=True)
+print("Hello World!!")
