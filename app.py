752e89ccc5b834080074122ccb5b1b75353a6bca
JAIN Ankush <ankush.jain2@soprasteria.com>
2019-12-20 15:45:02 +0530

    Flask App Added

--- app.py
+++ app.py
@@ -8,4 +8,4 @@ def index():
     return "Welcome to student records API!" + "created by Ankush Jain"
 
 if __name__ == "__main__":
-    app.run(port=8000, use_reloader=True, debug=True)
+    app.run(port=8080, use_reloader=True, debug=True)
